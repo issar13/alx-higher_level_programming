3a4b071e3b357ba627fc38d3fdd757edaf0f8d87
issar13 <issa_suleiman008@hotmail.com>
2022-06-08 20:07:23 +0300

    over indented

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,11 +5,11 @@ def roman_to_int(roman_string):
     letter = {'I': 1, 'V': 5, 'X': 10,
               'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     total = 0
-    maxchar = 'I'
+    romachar = 'I'
     new_list = roman_string[::-1]
     for elm2 in new_list:
-        if letter[elm2] >= letter[maxchar]:
-            maxchar = elm2
+        if letter[elm2] >= letter[romachar]:
+            romachar = elm2
             total += letter[elm2]
         else:
             total -= letter[elm2]
