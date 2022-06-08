b27ec6af832227a14c52ce4740421adb09364136
issar13 <issa_suleiman008@hotmail.com>
2022-06-08 20:05:33 +0300

    over indented

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,8 +4,8 @@ def best_score(a_dictionary):
         value = 0
         for key in a_dictionary:
             if value < a_dictionary[key]:
-                    value = a_dictionary[key]
-                    user = key
+                value = a_dictionary[key]
+                user = key
         return user
     else:
         return None
