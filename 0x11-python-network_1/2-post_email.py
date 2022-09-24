e1dba3cc6e1f8687685bc778c8d418598a41b470
issar13 <issa_suleiman008@hotmail.com>
2022-09-24 10:48:43 +0300

    lessgo

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
 """
-takes in a URL and an email, sends a POST request to the passed URL with
- the email as a parameter, and displays the body
- of the response (decoded in utf-8)
+POST request to the passed URL with the email as a parameter
 """
+import urllib.request
 import sys
 
-import urllib.request
 
-if __name__ == '__main__':
-    url = sys.argv[1]
-    value = {"email": sys.argv[2]}
-    data = url.parse.urlencode(value).encode("ascii")
+if __name__ == "__main__":
+    data = urllib.parse.urlencode({'email': sys.argv[2]})
+    data = data.encode('ascii')
+    reqq = urllib.request.Request(sys.argv[1], data)
 
-    request = urllib.request.Request(url, data)
-    with urllib.request.urlopen(request) as response:
-        print(response.read().decode("utf-8"))
+    try:
+        with urllib.request.urlopen(reqq) as resp:
+            if resp is not None:
+                html = resp.read()
+                print(html.decode('utf-8'))
+    except Exception:
+        pass
