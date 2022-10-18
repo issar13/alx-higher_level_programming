cf518382c6d9258496b38e0f3b2d3c6829544755
issar13 <issa_suleiman008@hotmail.com>
2022-10-18 14:31:59 +0300

    star wars character

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,28 +1,32 @@
 #!/usr/bin/node
+
+function order (characters, idx) {
+  if (idx >= characters.length) {
+    return;
+  }
+  request(characters[idx], function (err, response, body) {
+    if (err) {
+      console.log(err);
+    } else if (response.statusCode === 200) {
+      const person = JSON.parse(body);
+      console.log(person.name);
+      return order(characters, ++idx);
+    } else {
+      console.log('error ocurred, Status code: ' + response.statusCode);
+    }
+  });
+}
+
 const request = require('request');
-const { argv } = require('process');
-
-const BaseUrl = 'https://swapi-api.hbtn.io/api/films/';
-function MakeRequest (url) {
-  return new Promise(function (resolve, reject) {
-    request(url, (error, response, body) => {
-      if (!error && response.statusCode === 200) {
-        resolve(body);
-      } else {
-        reject(error);
-      }
-    });
-  });
-}
-
-async function main () {
-  const movie = await MakeRequest(BaseUrl + argv[2]);
-  const characters = JSON.parse(movie).characters;
-  characters.forEach(async function (element) {
-    const character = await MakeRequest(element);
-    const CharacterName = JSON.parse(character).name;
-    console.log(CharacterName);
-  });
-}
-
-main();
+const url = 'https://swapi-api.hbtn.io/api/films/';
+const ep = process.argv[2];
+request(url + ep, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const jsobj = JSON.parse(body);
+    order(jsobj.characters, 0);
+  } else {
+    console.log('error ocurred, Status code: ' + response.statusCode);
+  }
+});
